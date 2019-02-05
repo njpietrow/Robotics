956fc15f0b43f3c2d6bd30303d9bd46ebf6426fa
Nick Pietrow <pietrow@usc.edu>
2019-02-04 17:09:43 -0800

    filled out pd_controller

--- lab1/lab4.py
+++ lab1/lab4.py
@@ -1,4 +1,6 @@
 from p_controller import PController
+from pd_controller import PDController
+import time
 class Run:
     def __init__(self, factory):
         self.create = factory.create_create()
@@ -7,12 +9,12 @@ class Run:
         self.servo = factory.create_servo()
         # define the gains here
         self.kp = 300
-        self.kd = ...
+        self.kd = 3
         self.minOutput = -500
         self.maxOutput = 500
         # instantiate your controllers here
-        self.p_controller = PController(self.kp, self.minOutput, self.maxOutput)
-        # self.pd_controller = PDController(self.kp, self.kd, self.minOutput, self.maxOutput)
+        #self.p_controller = PController(self.kp, self.minOutput, self.maxOutput)
+        self.pd_controller = PDController(self.kp, self.kd, self.minOutput, self.maxOutput)
 
 
     def run(self):
@@ -20,16 +22,25 @@ class Run:
         self.create.safe()
 
         self.servo.go_to(70)
-        self.time.sleep(2)
+        self.time.sleep(3)
 
         goal_distance = 0.5
         base_speed = 100
+
+        prev_time = time.time()
+        prev_distance = goal_distance
 
         while True:
             distance = self.sonar.get_distance()
             if distance is not None:
                 print(distance)
-                vX, vY = self.p_controller.update(distance, base_speed, goal_distance)
+
+                change_time = time.time() - prev_time
+                change_distance = distance - prev_distance
+                vX, vY = self.pd_controller.update(distance, base_speed, goal_distance, change_distance, change_time)
+                # vX, vY = self.p_controller.update(distance, base_speed, goal_distance)
                 # print(str(vX) + ", " + str(vY))
                 self.create.drive_direct(vX,vY)
                 self.time.sleep(0.01)
+                prev_time = time.time()
+                prev_distance = distance
--- lab1/pd_controller.py
+++ lab1/pd_controller.py
@@ -1,23 +1,35 @@
 import math
-class PController:
-	def __init__(self, kp, minOutput, maxOutput):
+
+class PDController:
+	def __init__(self, kp, kd, minOutput, maxOutput):
 		self.kp = kp 
 		#constant gain
+		self.kd = kd
 		self.minOutput = minOutput
 		#minimum output value to limit calculated value
 		self.maxOutput = maxOutput
 		#maximum output value to limit calculated value
 		self.vLeft = 0
 		self.vRight = 0
-		self.IntegralLeftError = 0
-		self.IntegralRightError = 0
-	def update(self, currDistance, goalSpeed, goalDistance):
+
+		self.totalLeftError = 0
+		self.totalRightError = 0
+
+	def update(self, currDistance, goalSpeed, goalDistance, change_distance, change_time):
 		dError = currDistance - goalDistance
 		vLError = goalSpeed - self.vLeft 
 		vRError = goalSpeed - self.vRight
+		rateOfChange = change_distance/change_time
+		print("rate of change " + str(rateOfChange))
+		if(rateOfChange > 10.0):
+			rateOfChange = 1
+		elif(rateOfChange == 0):
+			rateOfChange = .1
+		
+
 		#if negative, need to go right
-		self.vLeft += (vLError + self.kp * dError)
-		self.vRight += (vRError - self.kp * dError)
+		self.vLeft += (vLError + self.kp*dError + self.kd*rateOfChange)
+		self.vRight += (vRError - self.kp*dError - self.kd*rateOfChange)
 
 		if(self.vLeft < self.minOutput):
 			self.vLeft = self.minOutput
@@ -29,4 +41,5 @@ class PController:
 			self.vRight = self.maxOutput
 
 		return self.vLeft , self.vRight
-		
+
+
