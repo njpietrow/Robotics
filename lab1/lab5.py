389f983c6315d78b641c7d935954cc11811a2748
Nick Pietrow <pietrow@usc.edu>
2019-02-11 16:26:00 -0800

    xy movement complete

--- lab1/lab5.py
+++ lab1/lab5.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 import odometry
 import pd_controller
+import pid_controller
 
 
 class Run:
@@ -19,7 +20,8 @@ class Run:
         self.servo = factory.create_servo()
         self.odometry = odometry.Odometry()
         self.pdTheta = pd_controller.PDController(500, 100, -200, 200)
-        
+        self.pidTheta = pid_controller.PIDController(500, 100, 300, -200, 200, -300, 300)
+
     def run(self):
         self.create.start()
         self.create.safe()
@@ -30,46 +32,63 @@ class Run:
             create2.Sensor.RightEncoderCounts,
         ])
 
-        goal_theta = ...  # TODO define it
-        # goal_x = ...
-        # goal_y = ...
+        # goal_theta = -math.pi/2
+        base_speed = 200
+        goal_x = .5
+        goal_y = .5
 
-        result = np.empty((0,3))
+        # rotate robot to theta and update x,y as moving
+        # goal theta is dependent on current xy and goal xy
+
+        # change result plot for the #4 has 5 data points, not 3
+        result = np.empty((0,5))
         end_time = self.time.time() + 10
         while self.time.time() < end_time:
             state = self.create.update()
             if state is not None:
                 self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
-                print("[%.6f, %.6f, %.6f]" % (self.odometry.x, self.odometry.y, math.degrees(self.odometry.theta)))
-                new_row = [self.time.time(), math.degrees(self.odometry.theta), math.degrees(goal_theta)]
-                result = np.vstack([result, new_row])
+                goal_theta = math.atan2(goal_y-self.odometry.y, goal_x-self.odometry.x)
 
-                # TODO call controller's update function 
+                if( abs((goal_y-self.odometry.y) + (goal_x-self.odometry.x)) < .01 ):
+                    break
+
+                print("[%.6f, %.6f, %.6f]" % (self.odometry.x, self.odometry.y, math.degrees(self.odometry.theta)))
+                new_row = [self.time.time(), math.degrees(self.odometry.theta), math.degrees(goal_theta),
+                           self.odometry.x, self.odometry.y]
+                result = np.vstack([result, new_row])
+                # print(goal_theta)
+                # print(self.odometry.theta)
+
+                # TODO call controller's update function
+                output = self.pidTheta.update(self.odometry.theta, goal_theta, self.time.time())
+
                 # and the self.create.drive_direct(left, right) here
+                self.create.drive_direct(int(base_speed + output), int(base_speed - output))
+
 
 
 
         # plotting for go-to-angle goal_theta:
-        plt.title("Angle")
-        plt.plot(result[:,0], result[:,1], label="odometry")
-        plt.plot(result[:,0], result[:,2], label="goal")
-        plt.grid()
-        plt.legend()
-        plt.savefig("lab6_angle.png") # make s ure to not overwrite plots
+        # plt.title("Angle")
+        # plt.plot(result[:,0], result[:,1], label="odometry")
+        # plt.plot(result[:,0], result[:,2], label="goal")
+        # plt.grid()
+        # plt.legend()
+        # plt.savefig("lab6_angle.png") # make s ure to not overwrite plots
 
         # plotting for go-to-goal (goal_x, goal_x):
-        # plt.figure()
-        # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-        # ax1.set_title("Angle")
-        # ax1.plot(result[:,0], result[:,1], label="odometry")
-        # ax1.plot(result[:,0], result[:,2], label="goal")
-        # ax1.grid()
-        # ax1.legend()
+        plt.figure()
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+        ax1.set_title("Angle")
+        ax1.plot(result[:,0], result[:,1], label="odometry")
+        ax1.plot(result[:,0], result[:,2], label="goal")
+        ax1.grid()
+        ax1.legend()
 
-        # ax2.set_title("Position")
-        # ax2.plot(result[:,3], result[:,4], label="odometry")
-        # ax2.scatter([goal_x], [goal_y], color="r", s=40, label="goal")
-        # ax2.axis("equal")
-        # ax2.grid()
-        # ax2.legend()
-        # plt.savefig("lab6_position.png")
+        ax2.set_title("Position")
+        ax2.plot(result[:,3], result[:,4], label="odometry")
+        ax2.scatter([goal_x], [goal_y], color="r", s=40, label="goal")
+        ax2.axis("equal")
+        ax2.grid()
+        ax2.legend()
+        plt.savefig("lab6_position.png")
--- lab1/p_controller.py
+++ lab1/p_controller.py
@@ -1,31 +1,15 @@
+#!/usr/bin/env python
 import math
+
+
 class PController:
-	def __init__(self, kp, minOutput, maxOutput):
-		self.kp = kp 
-		#constant gain
-		self.minOutput = minOutput
-		#minimum output value to limit calculated value
-		self.maxOutput = maxOutput
-		#maximum output value to limit calculated value
-		self.vLeft = 0
-		self.vRight = 0
-		self.IntegralLeftError = 0
-		self.IntegralRightError = 0
-	def update(self, currDistance, goalSpeed, goalDistance):
-		dError = currDistance - goalDistance
-		
-		#if negative, need to go right
-		self.vLeft = goalSpeed + self.kp*dError 
-		self.vRight = goalSpeed - self.kp*dError
+    def __init__(self, kp, min_output, max_output):
+        self.kp = kp
+        self.minOutput = min_output
+        self.maxOutput = max_output
 
-		if(self.vLeft < self.minOutput):
-			self.vLeft = self.minOutput
-		elif(self.vLeft > self.maxOutput):
-			self.vLeft = self.maxOutput	
-		if(self.vRight < self.minOutput):
-			self.vRight = -self.minOutput
-		elif(self.vRight > self.maxOutput): 
-			self.vRight = self.maxOutput
-
-		return self.vLeft , self.vRight
-		
+    def update(self, value, target_value):
+        error = target_value - value
+        p = self.kp * error
+        output = p
+        return max(min(output, self.maxOutput), self.minOutput)
